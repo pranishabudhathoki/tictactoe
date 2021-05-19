12eb41f16781d998e98bb8f0a6a1e9389023c3ce
pranisha budhathoki <pranishabudhathoki25@gmail.com>
2021-05-19 08:49:41 +0545

    this is first commit

---
+++ android/app/src/main/kotlin/com/pranisha/tictactoe_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pranisha.tictactoe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //to add icon image
+  var cross = Icon(
+    Icons.cancel,
+    size: 80,
+  );
+  var circle = Icon(
+    Icons.circle,
+    size: 80,
+  );
+  var edit = Icon(
+    Icons.edit,
+    size: 80,
+  );
+  bool isCross = true;
+  late String message;
+  late List<String> gameState;
+
+  //initialize box with empty value
+  @override
+  void initState() {
+    gameState = List.filled(9, 'empty');
+    this.message = "";
+    super.initState();
+  }
+
+  //to play game
+  playGame(int index) {
+    if (this.gameState[index] == "empty") {
+      setState(() {
+        if (this.isCross) {
+          this.gameState[index] = "cross";
+        } else {
+          this.gameState[index] = "circle";
+        }
+        isCross = !isCross;
+        checkWin();
+      });
+    }
+  }
+
+  //reset game
+
+  resetGame() {
+    setState(() {
+      gameState = List.filled(9, 'empty');
+      this.message = "";
+    });
+  }
+
+  //to get icon image
+  Icon getIcon(String title) {
+    switch (title) {
+      case ('empty'):
+        return edit;
+        break;
+      case ('cross'):
+        return cross;
+        break;
+      case ('circle'):
+        return circle;
+        break;
+    }
+    return getIcon(title);
+  }
+
+  //to check for winning
+
+  checkWin() {
+    if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[1]) &&
+        (gameState[1] == gameState[2])) {
+      setState(() {
+        this.message = this.gameState[0] + 'win';
+      });
+    } else if ((gameState[3] != 'empty') &&
+        (gameState[3] == gameState[4]) &&
+        (gameState[4] == gameState[5])) {
+      setState(() {
+        this.message = this.gameState[3] + 'win';
+      });
+    } else if ((gameState[6] != 'empty') &&
+        (gameState[6] == gameState[7]) &&
+        (gameState[7] == gameState[8])) {
+      setState(() {
+        this.message = this.gameState[6] + 'win';
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[3]) &&
+        (gameState[3] == gameState[6])) {
+      setState(() {
+        this.message = this.gameState[0] + 'win';
+      });
+    } else if ((gameState[1] != 'empty') &&
+        (gameState[1] == gameState[4]) &&
+        (gameState[4] == gameState[7])) {
+      setState(() {
+        this.message = this.gameState[1] + 'win';
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[5]) &&
+        (gameState[5] == gameState[8])) {
+      setState(() {
+        this.message = this.gameState[2] + 'win';
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[4]) &&
+        (gameState[4] == gameState[8])) {
+      setState(() {
+        this.message = this.gameState[0] + 'win';
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[4]) &&
+        (gameState[4] == gameState[6])) {
+      setState(() {
+        this.message = this.gameState[2] + 'win';
+      });
+    } else if (!gameState.contains('empty')) {
+      setState(() {
+        message = "game draw";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TicTacToe'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: GridView.builder(
+                padding: EdgeInsets.all(10),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemCount: gameState.length,
+                itemBuilder: (context, i) => SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: MaterialButton(
+                        onPressed: () {
+                          this.playGame(i);
+                        },
+                        child: getIcon(this.gameState[i]),
+                      ),
+                    )),
+          ),
+          Text(
+            message,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Container(
+            child: MaterialButton(
+              minWidth: 200,
+              color: Colors.blue[800],
+              onPressed: () {
+                resetGame();
+              },
+              child: Text(
+                'Reset game',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Text("#flutterwithBRP")
+        ],
+      ),
+    );
+  }
+}
